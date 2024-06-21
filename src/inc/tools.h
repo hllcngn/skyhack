a5f714622d49a405c9331540c96588cdf37810ad
d0pelrh <jsshscifi@gmail.com>
2024-06-21 10:54:39 +0200

    clearing branch main

--- src/inc/tools.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "ogns.h"
-
-void clear_window(SDL_Renderer* renderer, SDL_Color color);
--- src/tools.c
+++
@@ -1,5 +0,0 @@
-#include "ogns.h"
-
-void clear_window(SDL_Renderer* renderer, SDL_Color color){
-SDL_SetRenderDrawColor(renderer, color.r,color.g,color.b,color.a);
-SDL_RenderClear(renderer);	return;}
