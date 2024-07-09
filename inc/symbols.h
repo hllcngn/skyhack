50be4b1aace87b89a6ef2ea08c33059c5ecc2a0e
d0pelrh <realhllcngn@gmail.com>
2024-07-10 00:48:52 +0200

    cleaned up generate.c

--- inc/symbols.h
+++ inc/symbols.h
@@ -1,14 +1,15 @@
 #ifndef SYMBOLS_H
 #define SYMBOLS_H
 
-#define C_PLAYER 'G'	//G/@/A/
+#define C_PLAYER	'G'	// G/@/A/
 
-#define FLOOR_TYPE 'p'	//p/s
-#define PAVING '/'	//|/+//
-#define C_FLOOR '.'
+#define FLOOR_TYPE	's'	// s/p
+#define PAVING		'+'	// |/+//
+#define C_FLOOR		'.'
 
-#define OUTWALL_HL 'n'	//y/n
-#define WALL_HL 'y'	//y/n
-#define C_WALL '.'	//./+/|/
+#define OUTWALL_HL	'n'	// y/n
+#define WALL_HL		'y'	// y/n
+#define C_WALL		'H'	// ./+/|/X/
+#define C_WALL2		'X'
 
 #endif
--- src/generate.c
+++ src/generate.c
@@ -1,40 +1,27 @@
 #include "h.h"
-void	make_NS_wall(int y,int x, int n, char *clsn);
-void	make_WE_wall(int y,int x, int n, char *clsn);
 
 	// v seems like I could improve this function
 void	make_static_walls(char *clsn){
 if(WALL_HL =='y')
 attron(A_REVERSE);
 
-make_NS_wall(1,15, 8, clsn);
-make_WE_wall(8,1, 14, clsn);
 //top left corner room
-//vspace(1,15, ' ', 8); //rooms shouldn't be much smaller
-//space(8,1, ' ', 14);		//than 8x14
+vspace(1,15, C_WALL, 8); //rooms shouldn't be much smaller
+space(8,1, C_WALL, 14);		//than 8x14
 //random walls bottom left
-space(35,23, ' ', 58); //corridor
-vspace(19,23, ' ', LINES-1-19); //large left room
+space(35,23, C_WALL, 58); //corridor
+vspace(19,23, C_WALL, LINES-1-19); //large left room
 //room in the middle
-box_space(13,79, ' ', 10,20);
+box_space(13,79, C_WALL, 10,20);
 //connecting middle room and bottom left
-vspace(13+10,23+58+2, ' ', LINES-1-23);
+vspace(13+10,23+58+2, C_WALL, LINES-1-23);
 
 if(WALL_HL =='y')
 attroff(A_REVERSE);}
 
 
-void	make_WE_wall(int y,int x, int n, char *clsn){
-//A_REVERSE check here?
- space( y,x, C_WALL, n);
- //space_buf( y,x, 'X', n, clsn);
-}
-void	make_NS_wall(int y,int x, int n, char *clsn){
- vspace(y,x, C_WALL, n);
-// vspace_buf(clsn, y,x, 'X', n);
-}
-
 void	make_random_walls(){
+if(WALL_HL =='y')
 attron(A_REVERSE);
 
 int	minroomsize =4; //= cupboard
@@ -44,8 +31,9 @@ randsize.x =rrange(minroomsize, COLS/4);
 vect	randcoord;
 randcoord.y =rn(LINES-1-randsize.y);
 randcoord.x =rn(COLS-1-randsize.x); //random room
-box_space(randcoord.y,randcoord.x, '.', randsize.y,randsize.x);
+box_space(randcoord.y,randcoord.x, C_WALL, randsize.y,randsize.x);
 //  todo: not overwrite the enclosing walls
-box_space(0,COLS-15, '+', 6,15); //test room
+box_space(0,COLS-15, C_WALL2, 6,15); //test room
 
+if(WALL_HL =='y')
 attroff(A_REVERSE);}
